f936833562a9b09050a825de72dd2aa404b3bf47
Guilherme Borges Dias <guilhermebdias@live.com>
2025-12-06 12:43:26 +0100

    Fix Docker daemon startup and user group modification

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,10 +9,10 @@ log() {
 
 # Start docker daemon. The script should've been put here by the DinD devcontainer feature
 log "Starting Docker Daemon"
-sudo /usr/local/share/docker-init.sh
+/usr/local/share/docker-init.sh
 #
 # Add our user to docker group 
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 
 log "Installing docker provider with default options"
 devpod provider add docker
